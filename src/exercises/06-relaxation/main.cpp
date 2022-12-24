dc86b5b30f0fd571deb1f1c2062ab8e2a7da00d1
Iago Mendes <iagobrazmendes@gmail.com>
2022-12-24 13:44:00 -0300

    Debug.

--- src/exercises/06-relaxation/main.cpp
+++ src/exercises/06-relaxation/main.cpp
@@ -11,67 +11,22 @@ RoundSphereDyad dyad(chi);
 shared_ptr<Grid3DFunction> e_theta(nullptr);
 shared_ptr<Grid3DFunction> e_phi(nullptr);
 
-double get_e_theta(int i, int j, char coordinate) {
-	double theta = grid.theta(i);
-	double phi = grid.phi(j);
-
-	switch (coordinate) {
-		case 'x':
-			return dyad.e_theta_x(theta, phi);
-			break;
-		
-		case 'y':
-			return dyad.e_theta_y(theta, phi);
-			break;
-
-		case 'z':
-			return dyad.e_theta_z(theta, phi);
-			break;
-		
-		default:
-			return -1;
-			break;
-	}
-}
-
-double get_e_phi(int i, int j, char coordinate) {
-	double theta = grid.theta(i);
-	double phi = grid.phi(j);
-
-	switch (coordinate) {
-		case 'x':
-			return dyad.e_phi_x(theta, phi);
-			break;
-		
-		case 'y':
-			return dyad.e_phi_y(theta, phi);
-			break;
-
-		case 'z':
-			return dyad.e_phi_z(theta, phi);
-			break;
-		
-		default:
-			return -1;
-			break;
-	}
-}
-
 // Returns residual.
 double find_solution(int N_theta, int N_phi) {
 	Grid grid_(N_theta, N_phi);
 	grid = grid_;
 
 	// Reset solutions
-	e_theta = make_shared<Grid3DFunction>(grid, get_e_theta);
-	e_phi = make_shared<Grid3DFunction>(grid, get_e_phi);
-
+	e_theta = dyad.get_theta_function(grid);
+	e_phi = dyad.get_phi_function(grid);
+	
 	return run_relaxation(e_theta, e_phi, get_commutator_rms);
 }
 
 int main() {
 	// Temporarily run for only one resolution.
-	find_solution(20, 4*20);
+	int N = 20;
+	find_solution(N, 4*N);
 	return 0;
 
 	ofstream convergence_output("./assets/convergence.csv");
