beb9ebe97954c77cb76c29d3913183c3903b823a
Iago Mendes <iagobrazmendes@gmail.com>
2022-12-30 12:07:09 -0300

    Test more the embedding of ellipsoid.

--- src/exercises/08-embedding/main.cpp
+++ src/exercises/08-embedding/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 double chi = 0;
 
 // Dimensions of ellipsoid
-double a = 2;
+double a = 1;
 double b = 1;
 double c = 1;
 
@@ -23,20 +23,22 @@ void find_solution(int N_theta, int N_phi) {
 
 	// shared_ptr<RoundSphereMetric> metric = make_shared<RoundSphereMetric>(chi);
 	// shared_ptr<HorizonMetric> metric = make_shared<HorizonMetric>(chi);
-	// shared_ptr<EllipsoidMetric> metric = make_shared<EllipsoidMetric>(a, b, c);
-	shared_ptr<PeanutMetric> metric = make_shared<PeanutMetric>(d);
+	shared_ptr<EllipsoidMetric> metric = make_shared<EllipsoidMetric>(a, b, c);
+	// shared_ptr<PeanutMetric> metric = make_shared<PeanutMetric>(d);
 
 	shared_ptr<Grid3DFunction> e_theta = make_shared<Grid3DFunction>(grid);
 	shared_ptr<Grid3DFunction> e_phi = make_shared<Grid3DFunction>(grid);
 	shared_ptr<Grid3DFunction> embedding = make_shared<Grid3DFunction>(grid);
 
+	double identifier = a*100 + b*10 + c;
+
 	run_factorization(metric, e_theta, e_phi);
-	run_relaxation(e_theta, e_phi, get_commutator_rms, d);
+	run_relaxation(e_theta, e_phi, get_commutator_rms, identifier);
 	// run_relaxation(e_theta, e_phi, get_commutator_rms);
 	run_integration(e_theta, e_phi, embedding);
 
 	char embedding_filename[50];
-	sprintf(embedding_filename, "./assets/embedding_%.1f.csv", d);
+	sprintf(embedding_filename, "./assets/embedding_%.1f.csv", identifier);
 	// sprintf(embedding_filename, "./assets/embedding.csv");
 	ofstream embedding_output(embedding_filename);
 	for (int i = 0; i < grid.N_theta; i++) {
@@ -51,7 +53,7 @@ void find_solution(int N_theta, int N_phi) {
 
 int main() {
 	// Temporarily run for only one resolution.
-	// find_solution(50, 50);
+	// find_solution(15, 60);
 
 	// Vary grid space.
 	// for (int N = 10; N <= 100; N += 20) {
@@ -68,9 +70,16 @@ int main() {
 	// }
 
 	// Vary distance between spheres.
-	for (d = 1.2; d <= 1.4; d += 0.1) {
-		printf("\nd = %.1f\n", d);
+	// for (d = 1.2; d <= 1.4; d += 0.1) {
+	// 	printf("\nd = %.1f\n", d);
 
-		find_solution(50, 50);
+	// 	find_solution(50, 50);
+	// }
+
+	// Vary ellipsoid dimensions.
+	for (b = 1; b <= 9; b += 1) {
+		printf("\nb = %.1f\n", b);
+
+		find_solution(15, 60);
 	}
 }
